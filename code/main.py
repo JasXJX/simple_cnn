db54dbc3d9be763c723324f6b4881c4557e35f62
Jason Xu <55168153+JasXJX@users.noreply.github.com>
2023-11-27 22:18:08 -0500

    Added support for gpu acc on Mac

--- code/main.py
+++ code/main.py
@@ -6,7 +6,12 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 torch.manual_seed(3)
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+if torch.cuda.is_available() :
+    device = 'cuda:0'
+elif torch.backends.mps.is_available():
+    device = 'mps'
+else:
+    device = 'cpu'
 
 
 def train(features_src: np.ndarray, labels_src: np.ndarray, model, ep_n, lr):
